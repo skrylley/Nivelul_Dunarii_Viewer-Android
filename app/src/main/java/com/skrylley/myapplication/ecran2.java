221efcb5198e157c6ecf4d5038a5ca7b814e63ad
skrylley <razvi.andrew@gmail.com>
2023-09-21 13:13:06 +0300

    Update test

--- app/src/main/java/com/skrylley/myapplication/ecran2.java
+++ app/src/main/java/com/skrylley/myapplication/ecran2.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ecran2 extends AppCompatActivity {
-
+// comentariu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
